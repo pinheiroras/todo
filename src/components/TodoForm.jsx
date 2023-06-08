564fad0fa6fa6d3b3a53342aa161be9a4ff6ecb3
R Alves Pinheiro <pinheiro.ras@gmail.com>
2023-06-08 11:45:40 -0300

    :bug: Salvando o state do obj -useState

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import { useState } from 'react'
 
 const TodoForm = () => {
+  const [value, setValue] = useState('')
+  const [category, setCategory] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!value || !setValue) return
+    setValue('')
+    setCategory('')
+  }
+
   return (
     <div className='todo-form'>
       <h2>Criar tarefa</h2>
-      <form>
-        <input type="text" placeholder='Digite o titulo'/>
-        <select>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder='Digite o titulo' 
+        value={value}
+        onChange={(e) => setValue(e.target.value)} />
+
+        <select value={category} onChange={(e) => setCategory(e.target.value)}>
           <option value="">Selecione</option>
           <option value="Trabalho">Trabalho</option>
           <option value="Pessoal">Pessoal</option>
