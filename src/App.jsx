aab905897244ac7e1b2ccb1f97a539e0b0ba255d
R. Alves Pinheiro <raimundo.sousa@defensoria.ce.def.br>
2023-06-16 07:10:29 -0300

    :fire: Layout para filtro e ordenação

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 import Todo from "./components/Todo";
 import TodoForm from "./components/TodoForm";
 import Search from './components/Search';
-
+import Filter from './components/Filter';
 
 function App() {
   const  [todos, setTodos] = useState([
@@ -45,10 +45,13 @@ function App() {
   }
 
   const [search, setSearch] = useState('')
+  const [filter, setFilter] = useState('All')
+  const [sort, setSort] = useState('Asc')
 
   return <div className='app'>
       <h1>Lista de Tarefas</h1>
       <Search search={search} setSearch={setSearch}/>
+      <Filter filter={filter} setFilter={setFilter} sort={sort}/>
       <div className="todo-list">
         {todos
         .filter((todo) => todo.text.toLowerCase().includes(search.toLowerCase()))
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,7 @@
 const Search = ({search, setSearch}) => {
   return (
     <div className="search">
-    <h2>Perquisar:</h2>
+    <h2>Pesquisar:</h2>
     <input type="text" value={search} onChange={(e) => setSearch(e.target.value)} placeholder="Digite para pesquisar..."/>
     </div>
   )
